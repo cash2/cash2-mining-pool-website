5df859570ff86b66c0215de277046990816ad333
Paul Aphivantrakul <aphivantrakul@gmail.com>
2022-04-30 22:44:03 +0700

    Initial commit of Cash2 pool website

---
+++ config.js
@@ -0,0 +1,13 @@
+var api = "https://pool.cash2.org:8119";
+
+var email = "cash2@protonmail.com";
+var telegram = "";
+var discord = "";
+
+var marketCurrencies = ["CASH2-BTC", "CASH2-USD"];
+
+var blockchainExplorer = "https://blocks.cash2.org";
+var transactionExplorer = "https://blocks.cash2.org";
+
+var themeCss = "themes/default.css";
+var defaultLang = 'en';
